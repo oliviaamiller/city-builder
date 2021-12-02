e30fdb063b0a27f193279d40b8cefb92df558cd4
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-02 15:25:13 -0800

    grabbed DOM elements for dropdown menus, logged to console, all working

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions and grab DOM elements
 
+const skylineDropdown = document.getElementById('skyline-dropdown');
+const waterfrontDropdown = document.getElementById('waterfront-dropdown');
+const castleDropdown = document.getElementById('castle-dropdown');
+
+console.log(skylineDropdown, waterfrontDropdown, castleDropdown);
+
+
 // let state
 
 // set event listeners 
