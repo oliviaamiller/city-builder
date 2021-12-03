21653b0e6497ae61aa719278fc9708afca58ef96
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-02 16:57:55 -0800

    TDD for createCountString, test passed

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,9 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { createCountString } from '../utils.js';
+
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -15,4 +18,15 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('createCountString returns a string describing the current count of each piece of state supplied as an argument', (expect) => {
+    const expected = `the skyline has changed 4 times, the waterfront has changed 5 times, the castle has changed 6 times.`;
+
+    const actual = createCountString(4, 5, 6);
+
+    expect.equal(actual, expected, 'returns a string with current count of image changes');
+
+
+
 });
