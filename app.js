228ef3b9604922d42de9834440471132c69f0ae5
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-02 16:00:19 -0800

    added event listeners to show images from dropdown menus, images are now being displayed

--- app.js
+++ app.js
@@ -8,12 +8,49 @@ const skylineImg = document.getElementById('skyline-img');
 const waterfrontImg = document.getElementById('waterfront-img');
 const castleImg = document.getElementById('castle-img');
 
-console.log(skylineImg, waterfrontImg, castleImg);
 
 
 // let state
 
+let skylineCount = 0;
+let waterfrontCount = 0;
+let castleCount = 0;
+
+
 // set event listeners 
+skylineDropdown.addEventListener('change', () => {
+    const select = skylineDropdown.value;
+
+    skylineImg.src = `./assets/city-${select}.png`;
+
+    skylineCount++;
+
+    displayStats();
+
+});
+
+waterfrontDropdown.addEventListener('change', () => {
+    const select = waterfrontDropdown.value;
+
+    waterfrontImg.src = `./assets/water-${select}.png`;
+
+    waterfrontCount++;
+
+    displayStats();
+
+});
+
+castleDropdown.addEventListener('change', () => {
+    const select = castleDropdown.value;
+
+    castleImg.src = `./assets/castle-${select}.png`;
+
+    castleCount++;
+
+    displayStats();
+
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
